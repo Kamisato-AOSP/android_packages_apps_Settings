139766632b8495d7aa4336936ed1cd8fbdc55328
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2026-01-05 13:24:40 +0100

    Settings: Move pif options from Developer Options to Kamisato Labs
    Change-Id: I137c9e51cd4b8e7ea68833e1973a15299d899aad

--- src/com/android/settings/KamisatoLabsFragment.java
+++ src/com/android/settings/KamisatoLabsFragment.java
@@ -1,12 +1,52 @@
 package com.android.settings;
 
+import android.app.Activity;
 import android.app.settings.SettingsEnums;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.preference.Preference;
 
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.development.PifDataPreference;
 
 public class KamisatoLabsFragment extends DashboardFragment {
 
     private static final String TAG = "KamisatoLabsFragment";
+    private static final String PIF_DATA_KEY = "pif_data_setting";
+
+    private ActivityResultLauncher<Intent> mPifFilePickerLauncher;
+    private PifDataPreference mPifDataPreference;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        mPifFilePickerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    Uri uri = result.getData().getData();
+                    Preference pref = findPreference(PIF_DATA_KEY);
+                    if (pref instanceof PifDataPreference) {
+                        ((PifDataPreference) pref).handleFileSelected(uri);
+                    }
+                }
+            }
+        );
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mPifDataPreference = findPreference(PIF_DATA_KEY);
+        if (mPifDataPreference != null) {
+            mPifDataPreference.setFilePickerLauncher(mPifFilePickerLauncher);
+        }
+    }
 
     @Override
     protected int getPreferenceScreenResId() {
